e483e53cae7b420bfa567ab1ba5996240d684ace
Buddhika Chathuranga <buddhika_c@3logixs.lk>
2019-08-26 11:43:57 +0530

    animations added and cliend and worker modules created

---
+++ src/app/worker/worker.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WorkerRoutingModule } from './worker-routing.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    WorkerRoutingModule
+  ]
+})
+export class WorkerModule { }
